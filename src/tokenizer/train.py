721fe2afc4139c548af7cdce36fb4d884e92a83e
Javier Mejia <javiermejiaman@gmail.com>
2025-12-01 17:46:13 -0500

    Segregate and document functions in file_utils

--- src/tokenizer/train.py
+++ src/tokenizer/train.py
@@ -1,15 +1,11 @@
 import os
 import src.config as C
 from tokenizers import ByteLevelBPETokenizer
-
-file_paths = [os.path.join(C.DATA_PATH, f)
-              for f in os.listdir(C.DATA_PATH) 
-              if os.path.isfile(os.path.join(C.DATA_PATH, f))
-]
+from utils.file_utils import list_files_paths
 
 tokenizer = ByteLevelBPETokenizer()
 
-tokenizer.train(files=file_paths, 
+tokenizer.train(files=list_files_paths(C.DATA_PATH), 
                 vocab_size=C.VOCAB_SIZE, 
                 min_frequency=C.MIN_FREQ, 
                 special_tokens=['<pad>', '<unk>', '<bos>', '<eos>']
--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -3,18 +3,60 @@ import re
 import config as C
 
 def get_model_path(serial):
-  return os.path.join(C.CHECKPOINTS_PATH, C.MODEL_NAME, f'serial_{ serial }.pt')
+  """Gets the model path.
+
+  Args:
+    serial (int): Serial number of the target model.
+
+  Returns:
+    str: Path to the model.
+  """
+
+  return os.path.join(C.CHECKPOINTS_PATH, 
+                      C.MODEL_NAME, 
+                      f'serial_{ serial }.pt')
 
 def get_model_latest_serial():
-  model_serials = [extract_model_serial(os.path.join(C.CHECKPOINTS_PATH, C.MODEL_NAME, f))
-                   for f in os.listdir(C.CHECKPOINTS_PATH, C.MODEL_NAME) 
-                   if os.path.isfile(os.path.join(C.CHECKPOINTS_PATH, C.MODEL_NAME, f))
+  """Gets the latest serial of the model.
+
+  Returns:
+    int: Latest serial of the model.
+  """
+
+  model_checkpoints_path = os.path.join(C.CHECKPOINTS_PATH, 
+                                        C.MODEL_NAME)
+  
+  model_serials = [extract_model_serial(f) 
+                   for f in list_files_paths(model_checkpoints_path)
   ]
 
   return max(model_serials)
 
-def extract_model_serial():
-  if(match := re.search(r'*.serial_(*\d).pt', C.MODEL_NAME)):
+def extract_model_serial(model_serial_name):
+  """Extract serial from the model name.
+  
+  Args:
+    model_serial_name: Name of the model to extract the serial from.
+  
+  Returns:
+    int: Serial number of the model.
+  """
+  if(match := re.search(r'*.serial_(*\d).pt', model_serial_name)):
     return int(match.group(1))
   else:
     return None
+
+def list_files_paths(dir):
+  """List file paths.
+
+  Args:
+    dir (str): Target directory to list.
+  
+  Returns:
+    list: List of file paths inside the directory.
+  """
+  
+  return [os.path.join(dir, f)
+          for f in os.listdir(dir) 
+          if os.path.isfile(os.path.join(dir, f))
+  ]
