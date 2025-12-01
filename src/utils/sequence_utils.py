f4e32556a7c162df03c1e2bc3dcf3c3860b68dd5
Javier Mejia <javiermejiaman@gmail.com>
2025-12-01 18:02:26 -0500

    Document sliding_window function

--- src/utils/sequence_utils.py
+++ src/utils/sequence_utils.py
@@ -2,4 +2,17 @@ import torch
 import config as C
 
 def slide_window(batch: torch.Tensor) -> torch.Tensor: 
+  """Truncate tokens to keep max sequence size.
+
+  The tokens are truncated at the beginning to keep the
+  most recently generated tokens for context.
+
+  Args:
+    batch (Tensor): shape (B, S) - Batch of token sequences.
+  
+  Returns:
+    Tensor: shape (B, S) if sequence length S <= MAX_SEQ_LENGTH,
+            else shape (B, MAX_SEQ_LENGTH) for truncated sequences.
+  """
+
   return (batch.size(1) > C.MAX_SEQ_LEN) if batch[:, -C.MAX_SEQ_LEN:] else batch
