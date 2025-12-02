9fabde0286a51609b7885874ee1be3490fd98420
Javier Mejia <javiermejiaman@gmail.com>
2025-12-02 00:47:00 -0500

    Segregate and document training code

--- src/train/train.py
+++ src/train/train.py
@@ -1,64 +1,9 @@
-import numpy as np
-import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import random_split
 import src.config as C
 from train.dataset import TextDataset
 import torch.nn.functional as F
-from torch.nn.utils import clip_grad_norm_
-from utils.file_utils import load_model, save_model
-from torch.optim import AdamW
 
-def get_model():
-  model = load_model()
-  
-  return model, AdamW(model.parameters(), lr=C.LEARNING_RATE)
-
-def get_data(train_ds, valid_ds):
-  return (
-    DataLoader(train_ds, 
-               batch_size=C.BATCH_SIZE, 
-               shuffle=True),
-    DataLoader(valid_ds, 
-               batch_size=C.BATCH_SIZE * 2, 
-               shuffle=True)
-  )
-
-def loss_batch(model, loss_func, xb, yb, opt=None):
-  logits = model(xb)
-  loss = loss_func(logits.view(-1, logits.size(-1)), yb.view(-1))
-
-  if opt is not None:
-    loss.backward()
-    clip_grad_norm_(model.parameters(), C.GRAD_CLIP)
-    opt.step()
-    opt.zero_grad()
-
-  return loss.item(), len(xb)
-
-def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
-
-  for epoch in range(epochs):
-    total_loss = 0
-    total_samples = 0
-
-    model.train()
-    for xb, yb in train_dl:
-      xb, yb = xb.to(C.DEVICE), yb.to(C.DEVICE)
-      losses, batch_size = loss_batch(model, loss_func, xb, yb, opt)
-      total_loss += losses * batch_size
-      total_samples += batch_size
-    
-    model.eval()
-    with torch.no_grad():
-      losses, batch_size = zip(
-        *[loss_batch(model, loss_func, xb.to(C.DEVICE), yb.to(C.DEVICE)) 
-          for xb, yb in valid_dl]
-      )
-    valid_loss = np.sum(np.multiply(losses, batch_size)) / np.sum(batch_size)
-
-    print(epoch, valid_loss)
-
-    save_model(model, opt, epoch, total_loss / total_samples)
+from train.utils import fit, get_data, get_model
 
 loss_func = F.cross_entropy
 
---
+++ src/train/utils.py
@@ -0,0 +1,99 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import src.config as C
+from torch.nn.utils import clip_grad_norm_
+from utils.file_utils import load_model, save_model
+from torch.optim import AdamW
+
+def get_model():
+  """Get latest model and optimizer.
+
+  Returns:
+    Model: Instance of the latest version of the model.
+    AdamW: Model optimizer.
+  """
+
+  model = load_model()
+  
+  return model, AdamW(model.parameters(), lr=C.LEARNING_RATE)
+
+def get_data(train_ds, valid_ds):
+  """Gets the training and validation data loaders.
+  
+  Args:
+    train_ds (TextDataset): Training dataset.
+    valid_ds (TextDataset): Validation dataset.
+  
+  Returns:
+    (DataLoader, DataLoader): Training and validation data loaders.
+  """
+
+  return (
+    DataLoader(train_ds, 
+               batch_size=C.BATCH_SIZE, 
+               shuffle=True),
+    DataLoader(valid_ds, 
+               batch_size=C.BATCH_SIZE * 2, 
+               shuffle=True)
+  )
+
+def _loss_batch(model, loss_func, xb, yb, opt=None):
+  """Calculate loss for a batch of sequences.
+  
+  Args:
+    model (Model): Model instance.
+    loss_func (function): Loss function to apply.
+    xb (Tensor): shape (B, S) - Batch of input token sequences
+                 to pass through the model.
+    yb (Tensor): shape (B, S) - Batch of target token sequences.
+  
+  Returns:
+    (float, int): Tuple with the loss and batch size.
+  """
+
+  logits = model(xb)
+  loss = loss_func(logits.view(-1, logits.size(-1)), yb.view(-1))
+
+  if opt is not None:
+    loss.backward()
+    clip_grad_norm_(model.parameters(), C.GRAD_CLIP)
+    opt.step()
+    opt.zero_grad()
+
+  return loss.item(), len(xb)
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+  """Trains and validates the model.
+  
+  Args:
+    epochs (int): Number of epochs to train.
+    model (Model): Model instance.
+    loss_func (function): Loss function to apply.
+    opt (AdamW): Optimizer to apply.
+    train_dl (DataLoader): Training data loader.
+    valid_dl (DataLoader): Validation data loader.
+  """
+
+  for epoch in range(epochs):
+    total_loss = 0
+    total_samples = 0
+
+    model.train()
+    for xb, yb in train_dl:
+      xb, yb = xb.to(C.DEVICE), yb.to(C.DEVICE)
+      losses, batch_size = _loss_batch(model, loss_func, xb, yb, opt)
+      total_loss += losses * batch_size
+      total_samples += batch_size
+    
+    model.eval()
+    with torch.no_grad():
+      losses, batch_size = zip(
+        *[_loss_batch(model, loss_func, xb.to(C.DEVICE), yb.to(C.DEVICE)) 
+          for xb, yb in valid_dl]
+      )
+    valid_loss = np.sum(np.multiply(losses, batch_size)) / np.sum(batch_size)
+
+    print(epoch, valid_loss)
+
+    save_model(model, opt, epoch, total_loss / total_samples)
