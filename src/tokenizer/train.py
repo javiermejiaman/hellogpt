1b07a25842d690c6dfdb86c715964f4155dee153
Javier Mejia <javiermejiaman@gmail.com>
2025-12-03 00:01:23 -0500

    Add log in tokenizer trainer

--- src/tokenizer/train.py
+++ src/tokenizer/train.py
@@ -2,6 +2,9 @@ import os
 import src.config as C
 from tokenizers import ByteLevelBPETokenizer
 from src.utils.file_utils import list_files_paths
+from src.logging import get_logger
+
+log = get_logger()
 
 def train_tokenizer_model():
   """Trains the tokenizer model.
@@ -11,14 +14,22 @@ def train_tokenizer_model():
   data is changed.
   """
 
-  tokenizer = ByteLevelBPETokenizer()
+  try:
+    tokenizer = ByteLevelBPETokenizer()
 
-  tokenizer.train(files=list_files_paths(C.DATA_PATH), 
-                  vocab_size=C.VOCAB_SIZE, 
-                  min_frequency=C.MIN_FREQ, 
-                  special_tokens=['<pad>', '<unk>', '<bos>', '<eos>']
-  )
+    tokenizer.train(files=list_files_paths(C.DATA_PATH), 
+                    vocab_size=C.VOCAB_SIZE, 
+                    min_frequency=C.MIN_FREQ, 
+                    special_tokens=['<pad>', '<unk>', '<bos>', '<eos>']
+    )
+    
+    os.makedirs(C.TOKENIZER_MODEL_PATH, exist_ok=True)
+
+    tokenizer.save_model(C.TOKENIZER_MODEL_PATH)
+
+    log.debug(f'Tokenizer model saved to "' + C.TOKENIZER_MODEL_PATH + '"')
   
-  os.makedirs(C.TOKENIZER_MODEL_PATH, exist_ok=True)
+  except Exception as e:
+    log.error(f'Failed to train tokenizer.')
 
-  tokenizer.save_model(C.TOKENIZER_MODEL_PATH)
+    raise e
