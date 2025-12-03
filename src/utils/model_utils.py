f3a1d8b4f0a41747039cfa28df69ebba661ebb42
Javier Mejia <javiermejiaman@gmail.com>
2025-12-03 14:54:35 -0500

    Add dependency injection to model utils

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,111 +1,116 @@
 import os
 import re
 import torch
-import src.config as C
+from src.config import Config
 from src.model.model import Model
 from src.utils.file_utils import list_files_paths
 from src.logging import get_logger
+from src.enums import ResourcePath as RP
 
-log = get_logger()
+class ModelUtils:
 
-def get_model_path(serial):
-  """Gets the model path.
+  def __init__(self, cfg: Config):
+    self.cfg = cfg
+    self.log = get_logger(cfg)
 
-  Args:
-    serial (int): Serial number of the target model.
+  def get_model_path(self, serial):
+    """Gets the model path.
 
-  Returns:
-    str: Path to the model.
-  """
+    Args:
+      serial (int): Serial number of the target model.
 
-  return os.path.join(C.CHECKPOINTS_PATH, 
-                      C.MODEL_NAME, 
-                      f'serial_{ serial }.pt')
+    Returns:
+      str: Path to the model.
+    """
 
-def get_model_latest_serial():
-  """Gets the latest serial of the model.
+    return os.path.join(RP.MODEL_STATE, 
+                        self.cfg.model_name, 
+                        f'serial_{ serial }.pt')
 
-  Returns:
-    int: Latest serial of the model.
-  """
+  def get_model_latest_serial(self):
+    """Gets the latest serial of the model.
 
-  model_checkpoints_path = os.path.join(C.CHECKPOINTS_PATH, 
-                                        C.MODEL_NAME)
-  
-  model_serials = [extract_model_serial(f) 
-                   for f in list_files_paths(model_checkpoints_path)
-  ]
+    Returns:
+      int: Latest serial of the model.
+    """
 
-  if (len(model_serials) == 0 or all(None == s for s in model_serials)):
-    return None
-
-  return max(s for s in model_serials if s is not None)
-
-def extract_model_serial(model_serial_name):
-  """Extract serial from the model name.
-  
-  Args:
-    model_serial_name: Name of the model to extract the serial from.
-  
-  Returns:
-    int: Serial number of the model.
-  """
-  if(match := re.search(r'.*serial_(\d*).pt', model_serial_name)):
-    return int(match.group(1))
-  else:
-    return None
-
-def load_model():
-  """Loads model.
-  
-  Returns:
-    Model: Model instance.
-  """
-
-  from src.environment import get_device
-  
-  model = Model()
-  
-  if serial := get_model_latest_serial():
-    checkpoint = torch.load(get_model_path(serial), map_location="cpu")
-    model.load_state_dict(checkpoint['model_state'])
-
-  return model.to(get_device())
-
-def save_model(model, optimizer, epoch, train_loss):
-  """Saves model.
-
-  Saves the model state, optimizer state, epoch and training 
-  loss of the latest epoch.
-  
-  Args:
-    model (Model): Model instance.
-    optimizer (AdamW): Model optimizer instance.
-    epoch (int): Current training epoch.
-    train_loss (float): Training loss of the last epoch.
-  """
-
-  try:
-    os.makedirs(os.path.join(C.CHECKPOINTS_PATH, C.MODEL_NAME), 
-                exist_ok=True)
+    model_checkpoints_path = os.path.join(RP.MODEL_STATE, 
+                                          self.cfg.model_name)
     
-    checkpoint = {
-      "model_state": model.state_dict(),
-      "optimizer_state": optimizer.state_dict(),
-      "epoch": epoch,
-      "loss": train_loss
-    }
-    
-    latest_serial = get_model_latest_serial()
-    next_serial = latest_serial + 1 if latest_serial else 1
-    model_path = get_model_path(next_serial)
-    
-    torch.save(checkpoint, model_path)
+    model_serials = [self.extract_model_serial(f) 
+                    for f in list_files_paths(model_checkpoints_path)
+    ]
 
-    log.debug(f'Model state saved to "' + model_path + '"')
-  
-  except Exception as e:
-    log.error(f'Failed to save model "' + C.MODEL_NAME 
-              + '" serial #' + next_serial + '.', exc_info=True)
+    if (len(model_serials) == 0 or all(None == s for s in model_serials)):
+      return None
+
+    return max(s for s in model_serials if s is not None)
+
+  def extract_model_serial(model_serial_name):
+    """Extract serial from the model name.
     
-    raise e
+    Args:
+      model_serial_name: Name of the model to extract the serial from.
+    
+    Returns:
+      int: Serial number of the model.
+    """
+    if(match := re.search(r'.*serial_(\d*).pt', model_serial_name)):
+      return int(match.group(1))
+    else:
+      return None
+
+  def load_model(self):
+    """Loads model.
+    
+    Returns:
+      Model: Model instance.
+    """
+
+    from src.environment import get_device
+    
+    model = Model()
+    
+    if serial := self.get_model_latest_serial():
+      checkpoint = torch.load(self.get_model_path(serial), map_location="cpu")
+      model.load_state_dict(checkpoint['model_state'])
+
+    return model.to(get_device())
+
+  def save_model(self, model, optimizer, epoch, train_loss):
+    """Saves model.
+
+    Saves the model state, optimizer state, epoch and training 
+    loss of the latest epoch.
+    
+    Args:
+      model (Model): Model instance.
+      optimizer (AdamW): Model optimizer instance.
+      epoch (int): Current training epoch.
+      train_loss (float): Training loss of the last epoch.
+    """
+
+    try:
+      os.makedirs(os.path.join(RP.MODEL_STATE, self.cfg.model_name), 
+                  exist_ok=True)
+      
+      checkpoint = {
+        "model_state": model.state_dict(),
+        "optimizer_state": optimizer.state_dict(),
+        "epoch": epoch,
+        "loss": train_loss
+      }
+      
+      latest_serial = self.get_model_latest_serial()
+      next_serial = latest_serial + 1 if latest_serial else 1
+      model_path = self.get_model_path(next_serial)
+      
+      torch.save(checkpoint, model_path)
+
+      self.log.debug(f'Model state saved to "' + model_path + '"')
+    
+    except Exception as e:
+      self.log.error(f'Failed to save model "' + self.cfg.model_name 
+                + '" serial #' + next_serial + '.', exc_info=True)
+      
+      raise e
